592be71cf5471ac4addbaa2c27c50ea8e6c3096f
aitegin <zhanybaev1211@gmail.com>
2022-08-04 14:55:34 +0600

    Background video changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
 import Routes from './Routes';
+const background = require('./assets/wave-cafe-video-bg.mp4')
 
 function App() {
   return (
     <div className="App">
-        <Routes/>
+      <video id="background-video" autoPlay loop muted style={{
+        width: '100vw', 
+        height: '100vh', 
+        objectFit: 'cover', 
+        position: 'fixed', 
+        left: 0, 
+        right: 0,
+        top: 0, 
+        bottom: 0, 
+        zIndex: -1
+      }} 
+      >
+        <source src={background.default} type="video/mp4"/>
+      </video>
+      <Routes/>
     </div>
   );
 }
