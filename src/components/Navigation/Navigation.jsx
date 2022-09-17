c5061e185244f8a91e752e6aec0c03754d061a42
aitegin <zhanybaev1211@gmail.com>
2022-09-17 14:36:49 +0600

    Showing active page

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,26 +2,28 @@ import React from 'react';
 import { FaUsers, FaCoffee, FaComments, FaMugHot } from 'react-icons/fa';
 import { RiLogoutBoxFill } from 'react-icons/ri'
 import { auth } from 'firebase';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import "../../Styles/styles.css"
 
 
 const Navigation = () => {
     const { handleLogout } = auth()
+
+    const {pathname} = useLocation()
     return (
-        <div className="navigation" style={{display:'flex', flexWrap:'wrap'}}>
+        <div className= "navigation" style={{display:'flex', flexWrap:'wrap'}}>
             <div className="waveCafe">
                 <FaCoffee id="coffee1"/>
                 Wave Cafe
             </div>
-            <NavLink style={{textDecoration:'none', color:'#099'}} to="/">
+            <NavLink className={pathname==='/'? 'activePage': ''} style={{textDecoration:'none', color:'#099'}} to="/">
             <div className="menu">
                 <FaMugHot className="icon"/>
                 Drink Menu
             </div>
             </NavLink>
 
-            <NavLink to="/about" >
+            <NavLink className={pathname==='/about'? 'activePage': ''} to="/about" >
                 <div className="about_us">
                     <FaUsers className="icon"/>
                     About Us
@@ -33,7 +35,7 @@ const Navigation = () => {
                 Logout
             </div>
             
-            <NavLink to='/contact' >
+            <NavLink className={pathname==='/contact'? 'activePage': ''} to='/contact' >
                 <div className="contacts">
                     <FaComments className="icon"/>
                     Contact
